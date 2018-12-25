75c65a64548ad1a57cf4d4c1327646f7df2ea914
Anshul Agrawal <Anshul.Agrawal@walmartlabs.com>
2018-12-25 19:17:44 +0530

    sms manager first cut

---
+++ SmsManager/app/src/main/java/com/example/a0a00uj/smsmanager/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a0a00uj.smsmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
