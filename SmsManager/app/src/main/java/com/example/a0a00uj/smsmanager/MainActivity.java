eb7fe6818e2560efb54e369358879f5c0d76a160
Anshul Agrawal <Anshul.Agrawal@walmartlabs.com>
2018-12-25 22:44:40 +0530

    added basic code for phone app intent, needs refactoring

--- SmsManager/app/src/main/java/com/example/a0a00uj/smsmanager/MainActivity.java
+++ SmsManager/app/src/main/java/com/example/a0a00uj/smsmanager/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.a0a00uj.smsmanager;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void makeCall(View view) {
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+
+        String phoneNumber = String.format("tel: %s",
+                textView.getText().toString());
+
+
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+
+
+        dialIntent.setData(Uri.parse(phoneNumber));
+
+
+        if (dialIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(dialIntent);
+        } else {
+            Log.e("OOPS", "Can't resolve app for ACTION_DIAL Intent.");
+        }
+    }
+
 }
